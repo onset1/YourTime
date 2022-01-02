ca2bc707575bee342aec332dc60ae8b59a1ae81e
onset1 <learningtoomuch@protonmail.com>
2022-01-01 23:29:48 -0300

    Mode colors

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,73 @@ function primary_function () {
 }
 
 setInterval(primary_function, 1000)
+
+// Change mode: Timer
+function timer_changer () {
+    // Changing colors (hue to -145)
+    const primary_color = "rgb(90, 2, 0)";
+    const secondary_color = "rgb(212, 136, 152)"
+    const header_color = "rgb(80, 2, 0)";
+    
+    document.getElementById("body").style.backgroundColor = primary_color;
+    
+    document.getElementById("header").style.backgroundColor = header_color;
+    
+    var hr_class = document.getElementsByClassName("hr");
+    for (var i = 0; i < hr_class.length; i++ ) {
+        hr_class[i].style.color = header_color;
+    };
+
+    document.getElementById("clock-button").className = "mode-button-timer";
+    document.getElementById("timer-button").className = "mode-button-timer";
+    document.getElementById("stopwatch-button").className = "mode-button-timer";
+
+}
+
+document.getElementById("timer-button").addEventListener("click", timer_changer);
+
+// Change mode: Clock
+function clock_changer () {
+    // Changing colors
+    const primary_color = "rgb(0, 90, 40)";
+    const secondary_color = "rgb(136, 212, 152)";
+    const header_color = "rgb(0, 80, 35)";
+    
+    document.getElementById("body").style.backgroundColor = primary_color;
+    
+    document.getElementById("header").style.backgroundColor = header_color;
+    
+    var hr_class = document.getElementsByClassName("hr");
+    for (var i = 0; i < hr_class.length; i++ ) {
+        hr_class[i].style.color = header_color;
+    }
+
+    document.getElementById("clock-button").className = "mode-button";
+    document.getElementById("timer-button").className = "mode-button";
+    document.getElementById("stopwatch-button").className = "mode-button";
+}
+
+document.getElementById("clock-button").addEventListener("click", clock_changer);
+
+// Change mode: Stopwatch
+function stopwatch_changer () {
+    // Changing colors (hue to +60)
+    const primary_color = "rgb(0, 49, 90)";
+    const secondary_color = "rgb(136, 195, 212)";
+    const header_color = "rgb(0, 43, 80)";
+    
+    document.getElementById("body").style.backgroundColor = primary_color;
+    
+    document.getElementById("header").style.backgroundColor = header_color;
+    
+    var hr_class = document.getElementsByClassName("hr");
+    for (var i = 0; i < hr_class.length; i++ ) {
+        hr_class[i].style.color = header_color;
+    }
+
+    document.getElementById("clock-button").className = "mode-button-stopwatch";
+    document.getElementById("timer-button").className = "mode-button-stopwatch";
+    document.getElementById("stopwatch-button").className = "mode-button-stopwatch";
+}
+
+document.getElementById("stopwatch-button").addEventListener("click", stopwatch_changer);
